3a022bd9074ed4482c732a7731b2933d5bfb5b51
Dougmega182 <djbbinteractive@gmail.com>
2025-03-05 23:49:22 +0000

    Initial commit

--- site-signin-backend/app/main.py
+++ site-signin-backend/app/main.py
@@ -1,11 +1,14 @@
 import os
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from typing import Optional
+import uvicorn
+
+app = FastAPI()
+
 
 # Load environment variables from .env
 load_dotenv()
-
-app = FastAPI()
 
 @app.get("/")
 def read_root():
@@ -14,3 +17,6 @@ def read_root():
         "database_url": os.getenv("DATABASE_URL"),
         "secret_key": os.getenv("SECRET_KEY"),
     }
+    
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
