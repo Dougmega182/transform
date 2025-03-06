ea7e2b2a0ad73c89b7572fc58094f0f88e75cc00
Dougmega182 <djbbinteractive@gmail.com>
2025-03-06 00:44:49 +0000

    Initial commit

--- site-signin-backend/app/main.py
+++ site-signin-backend/app/main.py
@@ -18,5 +18,6 @@ def read_root():
         "secret_key": os.getenv("SECRET_KEY"),
     }
     
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
+# python web server
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
