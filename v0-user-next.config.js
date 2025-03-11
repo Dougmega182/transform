db8e9f6a5367f2c107d0bdedf3ae787232d32810
Dougmega182 <djbbinteractive@gmail.com>
2025-03-11 16:49:23 +1100

    modified:   next.config.mjs 	modified:   package-lock.json 	modified:   v0-user-next.config.js

--- v0-user-next.config.js
+++ v0-user-next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const userNextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+};
 
-module.exports = nextConfig
-
+export default userNextConfig;
